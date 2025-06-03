72a4db5151088d167dde3f42080aef5494aa66db
Parker Jones <parker.jones@rithum.com>
2025-06-02 23:48:42 -0400

    better

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -1,55 +1,20 @@
-{ config, pkgs, secrets, ... }:
+{ config, pkgs, agenix, secrets, ... }:
 
-let user = "pjones"; in
-{
-  age = { 
-    identityPaths = [ 
-      "/Users/${user}/.ssh/parallaxis"
-    ];
-
+let
+  user = "pjones";
+in {
+  programs.agenix = {
+    enable = true;
+    identityPaths = [ "/Users/${user}/.ssh/parallaxis" ];
     secrets = {
       "openai-key" = {
-        symlink = true;   # “after decrypt, symlink into the target path”
+        symlink = true;
         path    = "/Users/${user}/.config/nvim/openai_key.txt";
-        file    = "${secrets}/openai-key.age";   # <— the “openai-key.age” inside your secrets repo
+        file    = "${secrets}/openai-key.age";
         mode    = "600";
         owner   = "${user}";
-        # group   = "staff";
+        group   = "staff";
       };
-      # "syncthing-cert" = {
-      #   symlink = true;
-      #   path = "/Users/${user}/Library/Application Support/Syncthing/cert.pem";
-      #   file =  "${secrets}/darwin-syncthing-cert.age";
-      #   mode = "644";
-      #   owner = "${user}";
-      #   group = "staff";
-      # };
-      #
-      # "syncthing-key" = {
-      #   symlink = true;
-      #   path = "/Users/${user}/Library/Application Support/Syncthing/key.pem";
-      #   file =  "${secrets}/darwin-syncthing-key.age";
-      #   mode = "600";
-      #   owner = "${user}";
-      #   group = "staff";
-      # };
-
-      # "github-ssh-key" = {
-      #   symlink = true;
-      #   path = "/Users/${user}/.ssh/id_github";
-      #   file =  "${secrets}/github-ssh-key.age";
-      #   mode = "600";
-      #   owner = "${user}";
-      #   group = "staff";
-      # };
-      #
-      # "github-signing-key" = {
-      #   symlink = false;
-      #   path = "/Users/${user}/.ssh/pgp_github.key";
-      #   file =  "${secrets}/github-signing-key.age";
-      #   mode = "600";
-      #   owner = "${user}";
-      # };
     };
   };
 }

6aac4814d8f5da889bdef9bdfd1dd4674e3c2fba
Parker Jones <parker.jones@rithum.com>
2025-06-11 19:28:37 -0400

    commented out one password command line env var setting

--- modules/shared/config/op-setup.sh
+++ modules/shared/config/op-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
 # Fetch your OpenAI API key from 1Password and export it
-OPENAI_API_KEY="$(op item get kn5awwfh4yl7pyishgagqpjore --field "api key" --reveal)"
-export OPENAI_API_KEY
+#OPENAI_API_KEY="$(op item get kn5awwfh4yl7pyishgagqpjore --field "api key" --reveal)"
+#export OPENAI_API_KEY
 
-TAVILY_API_KEY="$(op item get qp6kvptfur6gratnvshzna2ev4 --field "credential" --reveal)"
-export TAVILY_API_KEY
+#TAVILY_API_KEY="$(op item get qp6kvptfur6gratnvshzna2ev4 --field "credential" --reveal)"
+#export TAVILY_API_KEY
